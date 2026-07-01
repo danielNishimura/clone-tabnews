988a2cecba8428c7b04d2dfae5e725c505949106
danielnishimura <nishimurani@gmail.com>
2026-07-01 20:15:28 -0300

    add and  files

--- app/api/v1/status/route.js
+++ app/api/v1/status/route.js
@@ -1,5 +1,13 @@
+import database from "../../../../infra/database.js";
+
 export async function GET(request) {
-    return new Response("alunos do curso.dev são pessoas acima da média", {
-        status: 200,
+    const result = await database.query("SELECT 1 + 1 as sum;");
+    console.log(result.rows);
+    
+    return Response.json({
+        message: "alunos do curso.dev são pessoas acima da média",
+        database: database ? "conectado" : "desconectado"
+    }, {
+        status: 200
     });
 }
