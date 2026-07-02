8c2394f0d13ab724c6b383ad293ccd9acddb0279
danielnishimura <nishimurani@gmail.com>
2026-07-01 21:17:32 -0300

    add  file

--- app/api/v1/status/route.js
+++ app/api/v1/status/route.js
@@ -1,4 +1,4 @@
-import database from "../../../../infra/database.js";
+import database from "infra/database.js";
 
 export async function GET(request) {
     const result = await database.query("SELECT 1 + 1 as sum;");
